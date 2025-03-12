7c575459c41b5318b9dda6dc92cc864834e0c07a
Vraj <pvraj7515@gmail.com>
2025-03-12 15:45:45 -0400

    first commit

---
+++ myapp/mymodule/funcs.py
@@ -0,0 +1,23 @@
+"""
+funcs.py contains four math functions.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a * 1.0 / b
+
+
+def calculate_area(n):
+    return n * n
---
+++ tests/mymodule/test_funcs.py
@@ -0,0 +1,25 @@
+import pytest
+
+from myapp.mymodule.funcs import *
+
+
+@pytest.mark.easy_operation
+def test_add():
+    # This test will fail.
+    assert add(4, 8) == 12
+
+@pytest.mark.easy_operation
+def test_subtract():
+    assert subtract(3, 6) == -3
+
+@pytest.mark.difficult_operation
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+@pytest.mark.difficult_operation
+def test_divide():
+    assert divide(56, 8) == 7
+
+@pytest.mark.difficult_operation
+def test_calculate_area():
+    assert calculate_area(80) == 6400
